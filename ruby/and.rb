17aa363d3da72aed474b12eb678550801e7cff0c
EC2 Default User <ec2-user@ip-172-31-19-92.ap-northeast-1.compute.internal>
2022-08-06 14:39:20 +0000

    14

---
+++ ruby/and.rb
@@ -0,0 +1,12 @@
+hand = "グー"
+if hand == "グー"
+  puts "出したてはぐーグー"
+end
+
+if hand != "チョキ"
+  puts "出したではチョキではありません"
+end
+
+if (hand == "グー") || (hand == "パー")
+  puts "出したてはグーまたはパーです"
+end
---
+++ ruby/new.rb
@@ -0,0 +1,37 @@
+class Car
+
+   def move(direction, distance)
+     self.turn(direction)
+     self.run(distance)
+   end
+
+  def tune(direction)
+    puts "#{direction}に曲がります。"
+  end
+
+  def run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+car = Car.new
+car.turn("右")
+
+car = Car.new
+car.run(5)
+
+class Car
+  def self.run(distance)
+    puts "車で#{distance}キロ走ります"
+  end
+end
+
+car = Car.new
+car.move("右", 5)
+
+
+:
+:
+
+
+
